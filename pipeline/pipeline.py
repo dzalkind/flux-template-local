97b75f8f1b720de0f423076fc057fd4f120884af
dzalkind <dzalkind@nrel.gov>
2021-10-13 12:17:51 -0600

    Add surface flux (sonic) config, plotting

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -72,22 +72,19 @@ class Pipeline(IngestPipeline):
         
         # Compress row of variables in input into variables dimensioned by time and height
         for raw_filename, raw_dataset in raw_mapping.items():
-            if ".sta" in raw_filename:
-                raw_categories = ["Wind Speed (m/s)", "Wind Direction (�)", "Data Availability (%)"]
-                output_var_names = ["wind_speed", "wind_direction", "data_availability"]
+            if "nwtc.flux_z01" in raw_filename:
+                raw_categories = ["U_ax","V_ax","W_ax","Ts"]
+                output_var_names = ["U_ax", "V_ax", "W_ax", "Ts"]
                 heights = dataset.height.data
                 for category, output_name in zip(raw_categories, output_var_names):
-                    var_names = [f"{height}m {category}" for height in heights]
+                    var_names = [f"{category}_{height}m" for height in heights]
                     var_data = [raw_dataset[name].data for name in var_names]
                     var_data = np.array(var_data).transpose()
                     dataset[output_name].data = var_data
 
-        # Apply correction to buoy at morro bay -- wind direction is off by 180 degrees
-        if "morro" in dataset.attrs["datastream_name"]:
-            new_direction = dataset["wind_direction"].data + 180
-            new_direction[new_direction >= 360] -= 360
-            dataset["wind_direction"].data = new_direction
-            dataset["wind_direction"].attrs["corrections_applied"] = "Applied +180 degree calibration factor."
+                # wind speed and direction, relative to U direction
+                dataset['wind_speed'] = np.sqrt(dataset.U_ax**2 + dataset.V_ax**2)
+                dataset['wind_direction'] = np.degrees(np.arctan2(dataset.V_ax,dataset.U_ax))
 
         return dataset
 
@@ -157,67 +154,39 @@ class Pipeline(IngestPipeline):
         avail_cmap = cmocean.cm.amp_r
 
         # Create the first plot - Lidar Wind Speeds at several elevations
-        filename = DSUtil.get_plot_filename(dataset, "wind_speeds", "png")
+        filename = DSUtil.get_plot_filename(dataset, "wind_speed_and_dir", "png")
         with self.storage._tmp.get_temp_filepath(filename) as tmp_path:
 
             # Create the figure and axes objects
-            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(14, 8), constrained_layout=True)
-            fig.suptitle(f"Wind Speed Time Series at {ds.attrs['location_meaning']} on {date}")
+            fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(14, 8), constrained_layout=True)
+            fig.suptitle(f"Wind Speed and Direction Time Series at {ds.attrs['location_meaning']} on {date}")
 
             # Select heights to plot
-            heights = [40, 90, 140, 200]
+            heights = [3,7]
 
-            # Plot the data
+            # Plot the wind speed
             for i, height in enumerate(heights):
                 velocity = ds.wind_speed.sel(height=height)
-                velocity.plot(ax=ax, linewidth=2, c=wind_cmap(i / len(heights)), label=f"{height} m")
+                velocity.plot(ax=ax[0], linewidth=2, c=wind_cmap(i / len(heights)), label=f"{height} m")
 
             # Set the labels and ticks
-            format_time_xticks(ax)
-            ax.legend(facecolor="white", ncol=len(heights), bbox_to_anchor=(1, -0.05))
-            ax.set_title("")  # Remove bogus title created by xarray
-            ax.set_xlabel("Time (UTC)")
-            ax.set_ylabel(r"Wind Speed (ms$^{-1}$)")
+            format_time_xticks(ax[0])
+            ax[0].legend(facecolor="white", ncol=len(heights), bbox_to_anchor=(1, -0.05))
+            ax[0].set_title("")  # Remove bogus title created by xarray
+            ax[0].set_xlabel("Time (UTC)")
+            ax[0].set_ylabel(r"Wind Speed (ms$^{-1}$)")
 
-            # Save the figure
-            fig.savefig(tmp_path, dpi=100)
-            self.storage.save(tmp_path)
-            plt.close()
-
-        filename = DSUtil.get_plot_filename(dataset, "wind_speed_and_direction", "png")
-        with self.storage._tmp.get_temp_filepath(filename) as tmp_path:
-
-            # Reduce dimensionality of dataset for plotting quivers
-            ds_1H: xr.Dataset = ds.resample(time="1H").nearest()
-
-            # Calculations for contour plots
-            levels = 30
-
-            # Calculations for quiver plot
-            qv_slice = slice(1, None)  # Skip first to prevent weird overlap with axes borders
-            qv_degrees = ds_1H.wind_direction.data[qv_slice].transpose()
-            qv_theta = (qv_degrees + 90) * (np.pi / 180)
-            X, Y = ds_1H.time.data[qv_slice], ds_1H.height.data
-            U, V = np.cos(-qv_theta), np.sin(-qv_theta)
-
-            # Create figure and axes objects
-            fig, axs = plt.subplots(nrows=2, figsize=(14, 8), constrained_layout=True)
-            fig.suptitle(f"Wind Speed and Direction at {ds.attrs['location_meaning']} on {date}")
-
-            # Make top subplot -- contours and quiver plots for wind speed and direction
-            csf = ds.wind_speed.plot.contourf(ax=axs[0], x="time", levels=levels, cmap=wind_cmap, add_colorbar=False)
-            axs[0].quiver(X, Y, U, V, width=0.002, scale=60, color="white", pivot='middle', zorder=10)
-            add_colorbar(axs[0], csf, r"Wind Speed (ms$^{-1}$)")
-
-            # Make bottom subplot -- heatmap for data availability
-            da = ds.data_availability.plot(ax=axs[1], x="time", cmap=avail_cmap, add_colorbar=False, vmin=0, vmax=100)
-            add_colorbar(axs[1], da, "Availability (%)")
+            # Plot the wind direction
+            for i, height in enumerate(heights):
+                direction = ds.wind_direction.sel(height=height)
+                direction.plot(ax=ax[1], linewidth=2, c=wind_cmap(i / len(heights)), label=f"{height} m")
 
             # Set the labels and ticks
-            for i in range(2):
-                format_time_xticks(axs[i])
-                axs[i].set_xlabel("Time (UTC)")
-                axs[i].set_ylabel("Height ASL (m)")
+            format_time_xticks(ax[1])
+            ax[1].legend(facecolor="white", ncol=len(heights), bbox_to_anchor=(1, -0.05))
+            ax[1].set_title("")  # Remove bogus title created by xarray
+            ax[1].set_xlabel("Time (UTC)")
+            ax[1].set_ylabel(r"Wind Direction (deg. relative to U)")
 
             # Save the figure
             fig.savefig(tmp_path, dpi=100)
